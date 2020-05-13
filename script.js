d778ddb02c55e1c343ac894aced5de46f8a8093f
zamanstrosity-cell <60775072+zamanstrosity-cell@users.noreply.github.com>
2020-05-13 16:28:13 +0200

    cart modal added

--- script.js
+++ script.js
@@ -6,32 +6,40 @@ const shop = document.querySelector(".items-container");
 
 let inventory = [];
 
-
+// factory function to build each item in the inventory
 const newItem = (name, price, imageSource) => {
-    return{name, price, imageSource};
+    var _itemName = name;
+    var _itemPrice = price;
+    var _itemImageSource = imageSource;
+    const getName = () => _itemName
+    const getPrice = () => _itemPrice
+    const getImageSource = () => _itemImageSource
+
+    return{getName, getPrice, getImageSource};
 }
 
+
+// adds new item to the inventory
 function addNewItem(name, price, imageSource){
     inventory.push(newItem(name, price, imageSource))
 }
 
 addNewItem('headphones', 30, './images/headphones.jpg');
 addNewItem('Speakers', 32, './images/speakers.png');
-addNewItem('Microphone', 56.99, './images/speakers.png');
-addNewItem('Lapto', 32, './images/speakers.png');
+addNewItem('Microphone', 56.99, './images/microphone.jpg');
+addNewItem('Laptop', 32, './images/laptop.jpg');
 
 
+// render the inventory to the page
 
-
-
-function addItem(inventory){
-    let name = item.name;
-    let price = item.price;
+function renderItem(item){
+    let name = item.getName();
+    let price = item.getPrice();
     const newDiv = document.createElement('div');
     newDiv.classList.add("item");
     let itemHtml = `
     <div class="image-container">
-    <img src=${item.imageSource} />
+    <img src=${item.getImageSource()} />
     </div>
     <div class="text-container">
     <h3>${name}</h3>
@@ -45,7 +53,39 @@ function addItem(inventory){
 
 
 inventory.forEach(item => {
-    addItem(item);
+    renderItem(item);
 });
+
+inventory.forEach(item => {
+    renderItem(item);
+});
+
+inventory.forEach(item => {
+    renderItem(item);
+});
+
+// code for modal
+
+const cartModal = (() =>{
+    //query selectors for modal
+    var modalSel = document.querySelector(".cart-modal")
+    var cartBtn = document.querySelector(".cart")
+    var closeModalBtn = document.querySelector(".close")
+
+    cartBtn.onclick = function(){
+        modalSel.style.display = "flex";
+    }
+
+    closeModalBtn.onclick = function(){
+        modalSel.style.display = "none";
+    }
+    
+    window.onclick = function(event) {
+        if (event.target == modalSel) {
+          modalSel.style.display = "none";
+        }
+      }
+})()
+
 
 
